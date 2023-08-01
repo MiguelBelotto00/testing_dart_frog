09e95eace3f31f9761ce105795a6a916bf61b139
Miguel Belotto <belottomiguel.00@gmail.com>
2023-07-31 20:06:25 -0400

    Second commit

--- routes/new_route.dart
+++ routes/new_route.dart
@@ -2,11 +2,12 @@ import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
   final request = context.request;
-  final method = request.method.value;
-  final headers = context.request.headers;
+  // final method = request.method.value; para saber el metodo: get,post,put,etc
+  // final headers = context.request.headers; para saber los headers.
+  final params = request.uri.queryParameters; // se trae los parametros.
+  final name = params['name'] ?? 'there';
 
-  final response =
-      'utiliza el metodo: $method y tiene estas cabeceras $headers';
+  final response = 'Hola $name, como te encuentras?\n';
 
   return Response(body: response);
 }
